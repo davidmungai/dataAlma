2e47e695a4f30865a3525d56dedac24e992f535b
davidMungai <mungahdaudi@gmail.com>
2021-08-12 04:14:04 +0300

    fix:added layout for website

---
+++ src/App.svelte
@@ -0,0 +1,388 @@
+<div class="container-center-horizontal">
+	<div class="home screen " data-id="aa5268f8-7635-422f-8d91-65767958da1c">
+		<img
+			class="bg-xl4bh6"
+			data-id="6bd807ef-8726-4c5c-9a3f-a5ec6cf20eef"
+			src="/img/bg@1x.png"
+			alt="imagsse shown here"
+		/>
+		<div
+			class="rectangle-555-xl4bh6"
+			data-id="1c8b56f5-6327-425b-a248-6f339bdd4675"
+		/>
+		<div
+			class="group-193-xl4bh6"
+			data-id="ba8ee713-150a-4150-ace3-2c127585d8b7"
+		>
+			<div
+				class="rectangle-474-RL6PBz"
+				data-id="9463bfef-ea6e-42bd-93c2-03ebfc7a5008"
+			/>
+			<div
+				class="try-free-RL6PBz"
+				data-id="581df6f4-6c84-4e95-8760-d91d14924405"
+			>
+				Try free
+			</div>
+		</div>
+		<div
+			class="group-155-xl4bh6"
+			data-id="f981bd39-8d69-4ba8-97c4-15933021544f"
+		>
+			<div
+				class="group-194-DNubbk"
+				data-id="34ac6700-5784-4e6e-9ee1-5aab5a1213b3"
+			>
+				<div
+					class="task-management-and-lists-tool-WPprpD"
+					data-id="433dc264-bc24-43c7-b7b5-73e608577ca6"
+				>
+					Task management and lists Tool
+				</div>
+				<div
+					class="group-154-WPprpD"
+					data-id="516d7699-0a98-4d31-bcd2-92c36f81ebb8"
+				>
+					<div
+						class="button-hV62B1"
+						data-id="843373d8-5d4c-4477-9c66-3a3d800cddd4"
+					>
+						<div
+							class="rectangle-454-piv4nl"
+							data-id="23f59bf2-c013-4c47-a088-fda187a12e84"
+						/>
+						<div
+							class="try-it-free-piv4nl poppins-semi-bold-white-23px"
+							data-id="76ce12f7-6838-489d-ba43-f0924b129641"
+						>
+							Try it free
+						</div>
+					</div>
+					<div
+						class="group-153-hV62B1"
+						data-id="4c6c96c8-60d4-4a28-8d0d-f4946887f47a"
+					>
+						<div
+							class="rectangle-455-F1lRCE"
+							data-id="dee8bfae-0bd5-4195-b3e2-856f15026acb"
+						/>
+						<div
+							class="namecompanycom-F1lRCE"
+							data-id="d445dee8-8d09-402c-8511-32a8b480386a"
+						>
+							Name@company.com
+						</div>
+					</div>
+				</div>
+				<div
+					class="x-WPprpD"
+					data-id="d3dc3765-e326-470e-bff2-d4d6250d301f"
+				>
+					&nbsp;
+				</div>
+			</div>
+		</div>
+		<div
+			class="we-believe-that-their-business-xl4bh6"
+			data-id="9dc36df6-05fa-43ab-b6d4-7f8d40e437a5"
+		>
+			We believe that designing products and services in close<br
+			/>partnership with our clients is the only way to have a real<br
+			/>impact on their business.
+		</div>
+		<div
+			class="why-do-you-need-gement-software-xl4bh6 poppins-bold-eerie-black-50px"
+			data-id="d079a67b-fa03-4540-a038-d25de19d8c8a"
+		>
+			Why do you need<br />task management software?
+		</div>
+		<div
+			class="seamless-team-w-task-management-xl4bh6 poppins-bold-eerie-black-50px"
+			data-id="efe4c08b-a008-4a05-a653-527b965bc7bc"
+		>
+			Seamless Team Workflow &amp; Efficient Task Management
+		</div>
+		<div
+			class="do-you-waste-ti-your-deadlines-xl4bh6"
+			data-id="7c1e81dd-2ff6-47c3-9cd9-58e1960cdba9"
+		>
+			Do you waste time organizing sticky notes, searching your email and
+			apps for to-dos, and figuring out what to work on first? Then you
+			need one solution to prioritize your tasks, manage your time, and
+			meet your deadlines.
+		</div>
+		<div
+			class="do-you-waste-ti-your-deadlines-efo9xP"
+			data-id="dd3632cd-b184-461e-b499-1657522031b5"
+		>
+			Do you waste time organizing sticky notes, searching your email and
+			apps for to-dos, and figuring out what to work on first? Then you
+			need one solution to prioritize your tasks, manage your time, and
+			meet your deadlines.
+		</div>
+		<div
+			class="featured-servic-that-we-provide-xl4bh6 poppins-bold-eerie-black-50px"
+			data-id="559832db-399a-4b74-a19c-e1487639b895"
+		>
+			Featured Service that We Provide
+		</div>
+		<div
+			class="trust-us-and-fe-try-our-service-xl4bh6 poppins-bold-eerie-black-50px"
+			data-id="ade8f34a-1a3f-43b0-9dcc-2cd66bd320e9"
+		>
+			Trust us and feel free to try our service
+		</div>
+		<div
+			class="get-better-work-done-xl4bh6 poppins-bold-eerie-black-50px"
+			data-id="6c770ea0-6d96-4432-b6be-df39e98093f3"
+		>
+			Get better work done
+		</div>
+		<div
+			class="group-238-xl4bh6"
+			data-id="d406c142-bd28-4de5-b123-9270e8a94ced"
+		>
+			<div
+				class="rectangle-556-xIrNjy"
+				data-id="41f297a9-d42f-48c6-a89d-bacccd7ca0f2"
+			/>
+			<div
+				class="group-218-xIrNjy"
+				data-id="67f7ba53-a8cc-4edf-9e47-417af8313b74"
+			>
+				<div
+					class="keep-tasks-inone-place-d5Rxxx"
+					data-id="2734c1ad-12e0-4c03-902e-e4bb6f6ff0ac"
+				>
+					Keep tasks in<br />one place
+				</div>
+				<p
+					class="save-time-avoid-tay-on-schedule-d5Rxxx"
+					data-id="9778466d-8cd1-40da-b9ee-ca3e58402bc3"
+				>
+					Save time, avoid losing work and information, delegate, and
+					track tasks to stay on schedule
+				</p>
+			</div>
+		</div>
+		<div
+			class="group-239-xl4bh6"
+			data-id="ba6f5d3f-b85a-4580-ab3e-8b84863267a6"
+		>
+			<div
+				class="rectangle-556-xCLrxT"
+				data-id="b6fa1bf8-bee3-4d36-bec8-68f753bf0535"
+			/>
+			<div
+				class="group-241-xCLrxT"
+				data-id="c833ef1d-5b94-499f-9067-fc1b217743c0"
+			>
+				<div
+					class="keep-tasks-inone-place-D9k95O"
+					data-id="ba8c9e0e-52bd-432e-adb8-6e346e48f382"
+				>
+					Keep tasks in<br />one place
+				</div>
+				<p
+					class="save-time-avoid-tay-on-schedule-D9k95O"
+					data-id="65a81ab6-8417-44f2-9a45-886d81ef7868"
+				>
+					Save time, avoid losing work and information, delegate, and
+					track tasks to stay on schedule
+				</p>
+			</div>
+		</div>
+		<div
+			class="group-240-xl4bh6"
+			data-id="ad5c224b-1c91-4611-a9d3-ab8d09126043"
+		>
+			<div
+				class="rectangle-556-txP3H0"
+				data-id="f0784337-0331-423b-8bd3-ab56d975e495"
+			/>
+			<div
+				class="group-242-txP3H0"
+				data-id="5dd62d1c-0bed-4ebb-9166-3ab9817623d3"
+			>
+				<div
+					class="keep-tasks-inone-place-x1pfdh"
+					data-id="03932674-2ed0-4d9a-bcda-eacf291ceff9"
+				>
+					Keep tasks in<br />one place
+				</div>
+				<p
+					class="save-time-avoid-tay-on-schedule-x1pfdh"
+					data-id="be9233c6-c2c1-45e2-83e8-72f367746e4c"
+				>
+					Save time, avoid losing work and information, delegate, and
+					track tasks to stay on schedule
+				</p>
+			</div>
+		</div>
+		<div
+			class="ellipse-23-xl4bh6"
+			data-id="79a92a9a-0267-455e-a755-48bb92ce2d6e"
+		/>
+		<div
+			class="group-228-xl4bh6"
+			data-id="e8f1135a-56a0-4632-ad76-21b867b7947c"
+		>
+			<img
+				class="mask-group-1-pxEx8f"
+				data-id="30bd02fc-4bfa-44a1-9e7a-f73070fbf3c7"
+				src="/img/mask-group-1@1x.png"
+				alt="imagsse shown here"
+			/>
+		</div>
+		<div
+			class="group-236-xl4bh6"
+			data-id="f37c182e-218f-45d2-abca-3bcd7a66a9ed"
+		>
+			<img
+				class="mask-group-1-z5ENNH"
+				data-id="5e675c2b-1fa3-4e11-b6a3-0e2081b5eb59"
+				src="/img/mask-group-1@1x.png"
+				alt="imagsse shown here"
+			/>
+		</div>
+		<div
+			class="group-237-xl4bh6"
+			data-id="f7eeb7f1-ee5d-4e63-bbe0-eac96cf3a10e"
+		>
+			<img
+				class="mask-group-1-dzLThN"
+				alt="imagsse shown here"
+				src="/img/mask-group-1-2@1x.png"
+			/>
+		</div>
+		<div
+			class="group-229-xl4bh6"
+			data-id="01154b95-afc8-4d21-a8d4-81d35894eed7"
+		>
+			<img
+				class="mask-group-1-FYmiTz"
+				alt="imagsse shown here"
+				src="/img/mask-group-1-3@1x.png"
+			/>
+		</div>
+		<div
+			class="group-230-xl4bh6"
+			data-id="a1582c2b-58ff-44f0-8606-ef3271dc5a40"
+		>
+			<img
+				class="mask-group-1-yJfgW4"
+				alt="imagsse shown here"
+				src="/img/mask-group-1-4@1x.png"
+			/>
+		</div>
+		<div
+			class="group-235-xl4bh6"
+			data-id="2751e10d-41b2-42c2-b8f0-4c1a8e2c775b"
+		>
+			<img
+				class="mask-group-1-AXeFKp"
+				alt="imagsse shown here"
+				src="/img/mask-group-1-5@1x.png"
+			/>
+		</div>
+		<div
+			class="see-why-million-ntries-use-dash-xl4bh6"
+			data-id="43f7aa8b-edf6-487e-ab55-de137664180e"
+		>
+			See why millions of people across 195 countries use Dash.
+		</div>
+		<div
+			class="group-234-xl4bh6"
+			data-id="b5d68be0-378b-4b9f-a1ea-8c5e14b973fa"
+		>
+			<div
+				class="group-153-tXqKjH"
+				data-id="a23c9616-1e5f-491e-802f-3925ad98a4d1"
+			>
+				<div
+					class="rectangle-455-nNlZOL"
+					data-id="e0cc7271-9b58-4c2d-98f4-106361824070"
+				/>
+				<div
+					class="namecompanycom-nNlZOL"
+					data-id="9c0e8109-9bd5-4cf4-b2f4-f59dc9edc867"
+				>
+					Name@company.com
+				</div>
+			</div>
+		</div>
+		<div
+			class="component-1-1-xl4bh6"
+			data-id="32a81289-3170-4213-aec3-b6f46af0aa08"
+		>
+			<div
+				class="rectangle-454-c411ja"
+				data-id="7e7eec63-9b39-48b2-a3bf-fb0e9c87dd56"
+			/>
+			<div
+				class="try-it-free-c411ja poppins-semi-bold-white-23px"
+				data-id="e183a690-0c23-4262-a8b4-6ac93fa4b9a2"
+			>
+				Try it free
+			</div>
+		</div>
+		<img
+			class="image-5-xl4bh6"
+			src="/img/image-5@1x.png"
+			alt="imagsse shown here"
+		/>
+		<div
+			class="component-1-2-xl4bh6"
+			data-id="7a33783c-2072-444b-900f-046929721166"
+		>
+			<div
+				class="rectangle-454-PmDV0P"
+				data-id="14b03a98-4c55-4dd5-93c6-79f62e67dbaa"
+			/>
+			<div
+				class="learn-more-PmDV0P poppins-semi-bold-white-23px"
+				data-id="c9803f38-82ed-449a-bfd5-544267477a4d"
+			>
+				Learn more
+			</div>
+		</div>
+		<h1 class="title-xl4bh6" data-id="1f4913d4-63e9-43a1-9b53-4bfe149735b4">
+			100 000
+		</h1>
+		<img
+			class="image-6-xl4bh6"
+			alt="imagssed shown here"
+			src="/img/image-6@1x.png"
+		/><img
+			class="image-7-xl4bh6"
+			src="/img/image-7@1x.png"
+			alt="imagsse shown here"
+		/><img
+			class="image-8-xl4bh6"
+			src="/img/image-8@1x.png"
+			alt="imagsse shown here"
+		/><img
+			class="image-9-xl4bh6"
+			src="/img/image-9@1x.png"
+			alt="imagsse shown here"
+		/><img
+			class="day80-tea-xl4bh6"
+			src="/img/day80-tea@1x.png"
+			alt="imagsse shown here"
+		/><img
+			class="day78-wallet-xl4bh6"
+			src="/img/day78-wallet@1x.png"
+			alt="imagsse shown here"
+		/><img
+			class="day77-pocket-knief-xl4bh6"
+			src="/img/day77-pocket-knief@1x.png"
+			alt="imagsse shown here"
+		/>
+		<div
+			class="taskia-xl4bh6"
+			data-id="94735fb7-a7c3-4664-befc-d5990231b3b6"
+		>
+			Taskia
+		</div>
+	</div>
+</div>
---
+++ src/Layout/Header.svelte
@@ -0,0 +1,61 @@
+<div class="header" />
+<div class="blue-section">
+    <div />
+    <div>
+        <span> info@dataalma.co.ke </span>
+        <span>254712345678</span>
+    </div>
+    <div>
+        <ul class="list-inline">
+            <li>
+                <a
+                    target="_blank"
+                    href="https://twitter.com/CodesoftK"
+                    class="twitter"
+                    title="Twitter"><i class="fa fa-twitter" /></a
+                >
+            </li>
+
+            <li>
+                <a
+                    target="_blank"
+                    href="https://www.facebook.com/codesoftLimitedSolutions/"
+                    class="facebook"
+                    title="Facebook"><i class="fa fa-facebook" /></a
+                >
+            </li>
+
+            <li>
+                <a target="_blank" href="#" class="youtube" title="YouTube"
+                    ><i class="fa fa-youtube" /></a
+                >
+            </li>
+
+            <li>
+                <a target="_blank" href="#" class="linkedin" title="LinkedIn"
+                    ><i class="fa fa-linkedin" /></a
+                >
+            </li>
+
+            <li>
+                <a target="_blank" href="#" class="instagram" title="Instagram"
+                    ><i class="fa fa-instagram" /></a
+                >
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="white-section">2</div>
+<div />
+
+<style>
+    .header {
+        color: #ffff;
+        display: grid;
+        grid-template-rows: 2fr 5fr;
+    }
+
+    .blue-section {
+        background-color: #3e56aa;
+    }
+</style>
